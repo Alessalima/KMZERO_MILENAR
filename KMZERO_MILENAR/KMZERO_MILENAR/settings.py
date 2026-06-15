79f214a7857b39bfcfee500767a54c3ef0bf0dbd
Alessalima <alessandra.silva@discente.ufra.edu.br>
2026-06-15 18:29:29 -0300

    segundo commit

--- KMZERO_MILENAR/KMZERO_MILENAR/settings.py
+++ KMZERO_MILENAR/KMZERO_MILENAR/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -114,4 +114,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [
+    'static',
+    'media',
+]
+
--- KMZERO_MILENAR/KMZERO_MILENAR/wsgi.py
+++ KMZERO_MILENAR/KMZERO_MILENAR/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
+from dj_static import Cling, MediaCling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "KMZERO_MILENAR.settings")
 
-application = get_wsgi_application()
+application = Cling(MediaCling(get_wsgi_application()))
